686b3c0631532571fa3b0f53027753081c69cd5c
Jeddsh <kandtramsey@gmail.com>
2023-04-07 00:31:27 -0400

    added sort.c and other related file

--- sort.c
+++ sort.c
@@ -1,20 +1,127 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
+//takes an input int array and builds a max heap from that.
+void heapify(int arr[], int n, int root){
+	int max = root;
+
+	int left = 2 * root + 1;
+	int right = 2 * root + 2;
+
+ 	if (left < n && arr[left] > arr[max]){
+        max = left;
+	}
+    
+	if (right < n && arr[right] > arr[max]){
+        max = right;
+	}
+    
+    if (max != root) {
+		int temp = arr[root];
+		arr[root]= arr[max];
+		arr[max] = temp;
+        heapify(arr, n, root);
+    }
+}
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+	for(int i = n/2-1; i >= 0; i--){
+		//builds a max heap
+		heapify(arr, n, i);
+	}
+	for(int i = n - 1; i >= 0; i--){
+		
+		//swaps first element with the last one
+		int temp = arr[i];
+		arr[i] = arr[0];
+		arr[0] = arr[i];
+
+		//creates a max heap out of the remaining values 
+		heapify(arr,n,i);
+	}
 }
 
+void merge(int arr[], int l, int m, int r){
+	//variables to be used later
+	int i, j, k;
+
+	//length of the left half (middle val - min value) +1 (0 index)
+	int n1 = m - l + 1;
+	//length of right half (max value - middle value)
+	int n2 = r-m;
+
+	//allocates extra memory for temparay arrays to merge
+	int *left = (int*) malloc(n1*sizeof(int));
+	extraMemoryAllocated++;
+	int *right = (int*)malloc(n2*sizeof(int));
+	extraMemoryAllocated++;
+	
+	//copies data from the inputed array from min value to the length of the left half.
+	for(i = 0; i < n1; i++){
+		left[i] = arr[l +i ];
+	}
+	//copies data from the inputed array from the length of the left half to the max value.
+	for(j = 0; j < n2; j++){
+		right[j] = arr[j + m + 1];
+	}
+
+	//merges temp arrays back together
+
+	i = 0;
+	j = 0;
+	k = l; //first index of the orginial array/merged array
+
+	while(i < n1 &&j < n2){
+		if(left[i]<right[j]){
+			arr[k] = left[i];
+			i++;
+		}
+		else{
+			arr[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+
+	//copies the remaining elements in the temp array
+	for(i;i<n1;i++){
+		arr[k] = left[i];
+		k++;
+	}
+	for(j;j<n2;j++){
+		arr[k] = right[j];
+		k++;
+	}
+
+	//frees the temporarily allocated memory
+	free(left);
+	free(right);
+}
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	//checks if left is less than right, else do nothing 
+	if(l < r){
+
+		//find midpoint
+		int m = (l + r)/2;
+
+		//recursively sort left half
+		mergeSort(pData,l,m);
+		//recursively sort right half
+		mergeSort(pData,m+1,r);
+
+		//merges the two halves
+		merge(pData,l,m,r);
+	}
 }
 
 // parses input file to an integer array
